ef9f2e1795c0d7fb9e1849c20cecc7d6a1606756
Vishnu V S <vishnusenthil222@gmail.com>
2024-01-02 11:55:13 +0530

    exercises 1 updated

--- 1.js
+++ 1.js
@@ -1,14 +1,17 @@
 let array = ['apple','banana','mango']
-array.map((fruit) => {
+array.forEach((fruit) => {
   console.log(fruit);
 }
 )
+console.log();
 
 let objectArray = [{fruit:'apple'},{fruit:'banana'},{fruit:'mango'}]
 objectArray.map((fruit) => {
   console.log(fruit.fruit);
 }
 )
+console.log();
+
 
 let object = { fruit1:'apple',fruit2:'banana',fruit3:'mango'}
 for(i in object){
--- 10.js
+++ 10.js
@@ -11,3 +11,4 @@ const a = {
   },
 };
 const b = a;
+
