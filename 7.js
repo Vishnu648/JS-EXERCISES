b91a4d55b8a5800166561f0cfe793984568d9c91
Vishnu V S <96856622+Vishnu648@users.noreply.github.com>
2024-01-03 10:00:31 +0530

    Update 7.js

--- 7.js
+++ 7.js
@@ -1,38 +1,38 @@
 const GLConfig = {
-    "CO": { type: "REQUIRED" },
-    "MAJ": { type: "NORMAL" },
-    "SET": { type: "NORMAL" },
-    "MIN": { type: "REQUIRED" }
+  CO: { type: "REQUIRED" },
+  MAJ: { type: "NORMAL" },
+  SET: { type: "NORMAL" },
+  MIN: { type: "REQUIRED" },
 };
 
 const GLSegments = {
-    "CO": "",
-    "MAJ": "111",
-    "SET": "111",
-    "MIN": "000"
+  CO: 's',
+  MAJ: "111",
+  SET: "111",
+  MIN: "000",
 };
-
-
-
-
 
 let validation = 0;
 
+
 for (let i in GLConfig) {
-    if (GLConfig[i].type == "REQUIRED") {
-        if (GLSegments[i].length <= i.length && GLSegments[i] != "" && GLSegments[i] != undefined &&
-            GLSegments[i] != null) {
-            validation = 1;
-        }
-        else {
-            validation = 0;
-            break;
-        }
+  if (GLConfig[i].type == "REQUIRED") {
+    if (
+      GLSegments[i].length <= i.length &&
+      GLSegments[i] != "" &&
+      GLSegments[i] != undefined &&
+      GLSegments[i] != null
+    ) {
+      validation = 1;
+    } else {
+      validation = 0;
+      break;
     }
+  }
 }
 
 if (validation == 1) {
-    console.log("-true");
+  console.log("true");
 } else {
-    console.log("-false");
+  console.log("false");
 }
